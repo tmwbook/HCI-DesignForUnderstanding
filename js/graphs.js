e4c2501e8b650541d87c11313491029daf30e001
Tom White <twhite@wpi.edu>
2018-04-01 01:25:06 -0400

    Listening and position updating first pass

--- js/graphs.js
+++ js/graphs.js
@@ -31,8 +31,15 @@ var pieChart = function(sketch) {
     this.testSort = [3, 5, 7, 2, 1, 9, 8]
     this.aux = []
 
-    this.leftGroup = []
-    this.rightGroup = []
+    this.filtered = false;
+
+    this.leftGroup = [];
+    this.rightGroup = [];
+
+    this.leftRow = 1;
+    this.leftCol = 1;
+    this.rightRow = 1;
+    this.rightCol = 1;
 /*
     this.merge = function(arr, low, mid, high) {
         var left = low;
@@ -70,10 +77,8 @@ var pieChart = function(sketch) {
     } 
 */
 
-    sketch.setup = () => {
+    sketch.setup = function() {
         sketch.createCanvas(height, width);
-        sketch.background(70, 70, 70);
-        sketch.noStroke();
         //mergeSort(testSort, 0, testSort.length);
     };
 
@@ -82,15 +87,79 @@ var pieChart = function(sketch) {
         //sketch.data
     };
 
-    sketch.draw = () => {
+    sketch.draw = function() {
+        sketch.background(70, 70, 70);        
         for(var i=0; i<activePoints.length; i++){
             p = activePoints[i];
             sketch.ellipse(p['loc'][0], p['loc'][1], pointRadius, pointRadius);
+
         }
     };
 
-    sketch.filterData = () => {
+    sketch.mouseClicked = function() {
+        if(sketch.mouseX < 0 || sketch.mouseY < 0 ||
+            sketch.mouseX > width || sketch.mouseY > height){
+                sketch.resetLists();
+                filtered = false;
+        } else if (!filtered){
+            sketch.filterData((x)=>{
+                return true;
+            })
+            filtered = true;
+        }
+    }
 
+    /**
+     * 
+     * @param {function} filter A boolean filter function
+     */
+    sketch.filterData = function(filter) {
+        for(var i=0; i<activePoints.length; i++){
+            if(filter(activePoints[i])){
+                leftGroup.push(activePoints[i]);
+            }else{
+                rightGroup.push(activePoints[i]);
+            }
+        }
+        sketch.postFilterMove(5);
+    };
+
+    /**
+     * 
+     * @param {Integer} maxCol the max number of columns in a filtered view
+     */
+    sketch.postFilterMove = function(maxCol){
+
+        for(var i=0; i<leftGroup.length; i++){
+            leftGroup[i]['loc'] = [20*leftCol, 20*leftRow];
+            leftCol++;
+            if(leftCol>maxCol){
+                leftCol=1;
+                leftRow++;
+            }
+        }
+        for(var i=0; i<rightGroup.length; i++){
+            rightGroup[i]['loc'] = [20*rightCol, width-(20*rightRow)];
+            rightCol++;
+            if(rightCol>maxCol){
+                rightCol=1;
+                rightRow++;
+            }
+        }
+    }
+
+    sketch.resetLists = function (){
+        for(var i=0; i<activePoints.length; i++){
+            var randX = Math.floor(Math.random() * (width+1));
+            var randY = Math.floor(Math.random() * (height+1));
+            activePoints[i]['loc'] = [randX, randY];
+        }
+        leftGroup = 1;
+        leftCol = 1;
+        rightGroup = 1;
+        rightCol = 1;
+        leftGroup = [];
+        rightGroup = [];
     }
 
 };
